3daf5c18a6ca6d270e668883919e616af2bfe753
Owadd <owadd001@gmail.com>
2024-06-28 19:27:38 +0100

    Updated changes

--- twitter_sentiment_analysis_app.py
+++ twitter_sentiment_analysis_app.py
@@ -22,7 +22,6 @@ def clean_tweet(text):
     text = re.sub(r"@\w+", "", text)
     text = re.sub(r"http\S+", "", text)
     text = re.sub(r"[^\w\s]", "", text)
-    text = " ".join(text.split())
     return text
 
 
